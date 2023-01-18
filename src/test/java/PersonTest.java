0df39159c8aaa2bbd22533030ee7270793a039c7
RowanPl <rowanplooij@hotmail.com>
2023-01-18 14:53:14 +0100

    Update

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class PersonTest {
+import org.example.Person;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PersonTest {
+
+@Test
+    public void getNameReturnsName(){
+    //Arrange
+    Person J = new Person("John", "Hendriks" , "Male", 30 );
+    //Act
+   String actual = J.getName();
+    //Assert
+    assertEquals("John", actual);
+}
+@Test
+    public void addChildToChildrenList(){
+    Person person = new Person("Jantje", "Pietersma" , "male", 30);
+    Person child = new Person("Jan", "Pietersma", "Male" ,12);
+
+    person.addChild(child);
+    List<Person> expected = person.getChildren();
+    assertEquals(List.of(child), expected );
+    }
+
+
 }
--- src/test/java/PetTest.java
+++ src/test/java/PetTest.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class PetTest {
+import org.example.Person;
+import org.example.Pet;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class PetTest {
+
+
+    @Test
+    public void addPetReturnName(){
+        //Arrange
+        Pet i = new  Pet("Henk", 23, "Dog");
+        //Act
+        String actual = i.getName();
+        //Assert
+        assertEquals("Henk", actual);
+
+
+    }
 }
